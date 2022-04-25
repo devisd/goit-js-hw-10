e55b086f45387023ae58d958766c384f2c8269e7
devisd <89788799+devisd@users.noreply.github.com>
2022-04-25 22:38:26 +0700

    test 1

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
-const URL = 'https://restcountries.com/v3.1/name/'
-
-function fetchCountries(inputName) {
-    return fetch(`${URL}${inputName}`)
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => response.json());
         
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,61 @@
 import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
-import countryCard from '../handlebars/country-card.hbs'
 import API from './fetchCountries';
-import { log } from 'handlebars';
 
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    container: document.querySelector('.country-info'),
+}
 
-const input = document.querySelector('#search-box');
-const list = document.querySelector('.country-list');
-const container = document.querySelector('.country-info');
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-input.addEventListener('input', debounce(onSearch,DEBOUNCE_DELAY))
+function onInput(e) {
+    if (e.target.value.length === 1 || e.target.value.length === 2) {
+        return Notify.info("Too many matches found. Please enter a more specific name.");
+    }
+    onSearch(e.target.value);
+}
 
-// Если пользователь полностью очищает поле поиска, то HTTP-запрос не выполняется, а разметка списка стран или информации о стране пропадает.
+function onSearch(inputValue) {
+    API.fetchCountries(inputValue)
+        .then(countryMarkup)
+        .then(listMarkup)
+        .catch(onError)
+}
 
-// Выполни санитизацию введенной строки методом trim(), это решит проблему когда в поле ввода только пробелы или они есть в начале и в конце строки.
+function countryMarkup(objects) {
+    return objects.map(object => {
+        if (objects.length > 1) {
+            refs.container.innerHTML = '';
+            return countryList(object);
+        }
+            refs.list.innerHTML = '';
+            const markup = oneContry(object);
+            refs.container.innerHTML = markup;
+    }).join('');
+}
+  
+function oneContry(e) {
+  return `<h2 style='display:flex' style='font-size:20px'><img src='${e.flags.svg}'>${e.name.official}</h2>
+            <p><b>Capital: </b>${e.capital}</p>
+            <p><b>Population: </b>${e.population}</p>
+            <p><b>Language: </b>${e.languages.eng}</p>`
+}
 
-// Notify.failure('Oops, there is no country with that name');
-// Notify.info('Too many matches found. Please enter a more specific name.');
+function countryList(e) {
+  return `<li><img src='${e.flags.svg}'>
+          <p style='font-size:20px'><b>${e.name.official}</b></p></li>`
+}
 
-function onSearch() {
-    const searchNameCountry = input.value;
-
-    container.innerHTML = '';
-
-    API.fetchCountries(searchNameCountry)
-        .then(country => {
-                const markup = countryCard(country)
-                console.log(markup);
-            container.innerHTML = markup;
-        }).catch(onError);
+function listMarkup(markup) {
+    refs.list.innerHTML = markup;
 }
 
 function onError() {
     Notify.failure('Oops, there is no country with that name');
+    refs.list.innerHTML = '';
+    refs.container.innerHTML = '';
 }
